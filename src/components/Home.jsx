678b7ec6c96a91e791b720511ac0cccc2fad6a55
AdeDevs <adeyemiakinyemi01@gmail.com>
2024-11-15 22:18:52 +0100

    updt

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,15 @@
-import { useState } from "react";
-
 function HomePage() {
-    const [activeNav, setActiveNav] = useState(false)
-
     return (
         <div className="home">
-            <h1 className={`test ${activeNav === 0 ? "active" : ""}`} onClick={() => setActiveNav(0)}>hello</h1>
-            <h1 className={`test ${activeNav === 1 ? "active" : ""}`} onClick={() => setActiveNav(1)}>hey there</h1>
-            <h1 className={`test ${activeNav === 2 ? "active" : ""}`} onClick={() => setActiveNav(2)}>be there</h1>
+            <header>
+                <div className="header-text">
+                    <button className="join-btn"> <span className="globe"><ion-icon name="earth-outline"></ion-icon></span> Join Us</button>
+                    <h1>Let's come <span className="color"> be part </span> of changes</h1>
+                    <button className="started-btn">Get Started</button>
+                    <button className="explore-btn"><span className="more-span">Explore More</span> <span><ion-icon name="arrow-forward-sharp"></ion-icon></span></button>
+                </div>
+                <div className="header-img"></div>
+            </header>
         </div>
     )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,45 +1,71 @@
 import { useState } from "react";
 
 function NavPage() {
-    const [showMenu, setShowMenu] = useState(false)
-    const [activeButton, isActiveButton] = useState(false)
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [linkActive, isLinkActive] = useState(null);
 
-    const toggleMenu = () => {
-        setShowMenu(!showMenu);
-    }
-    const toggleActiveButton = () => {
-        isActiveButton(!activeButton);
-    }
-    return (
-        <div className="navigation">
-            <nav>
-                <h1> <button><br /></button><button className="special"></button> Carryon</h1>
-                <ul className="desk-menu">
-                    <li>About</li>
-                    <li>Agenda</li>
-                    <li>Partnership</li>
-                    <li>Testimonial</li>
-                </ul>
-                <button className={`contact ${activeButton ? "active" : ""}`} onClick={toggleActiveButton}>Contact Us</button>
-                    <span className="toggle-menu" onClick={toggleMenu}>
-                        <ion-icon name="menu-outline" />
-                    </span>
-                    <div className={`overlay ${showMenu ? "show" : ""}`} onClick={toggleMenu}></div>
-                <div className={`hamburger-menu ${showMenu ? "show" : ""}`}>
-                    <span className="toggle-menu" onClick={toggleMenu}>
-                        <ion-icon name="close-outline" />
-                    </span>
-                    <ul>
-                        <li>About</li>
-                        <li>Agenda</li>
-                        <li>Partnership</li>
-                        <li>Testimonial</li>
-                        <li>Contact Us</li>
-                    </ul>
-                </div>
-            </nav>
+  const toggleCart = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  return (
+    <div className="navigation">
+      <nav>
+        <h1>
+          {" "}
+          <button>
+            <br />
+          </button>
+          <button className="special"></button> Carryon
+        </h1>
+        <ul className="desk-menu">
+          <li>About</li>
+          <li>Agenda</li>
+          <li>Partnership</li>
+          <li>Testimonial</li>
+        </ul>
+        <button className="contact">Contact Us</button>
+        <span className="toggle-menu" onClick={toggleCart}>
+          <ion-icon name="menu-outline" />
+        </span>
+
+        <div
+          className={`overlay ${isMenuOpen ? "show" : ""}`}
+          onClick={toggleCart}
+        ></div>
+        <div className={`hamburger-menu ${isMenuOpen ? "show" : ""}`}>
+          <span className="toggle-menu">
+            <ion-icon name="close-outline" onClick={toggleCart} />
+          </span>
+          <ul>
+            <li
+              className={linkActive === 0 ? "active" : ""}
+              onClick={() => isLinkActive(0)}
+            >
+              About
+            </li>
+            <li
+              className={linkActive === 1 ? "active" : ""}
+              onClick={() => isLinkActive(1)}
+            >
+              Agenda
+            </li>
+            <li
+              className={linkActive === 2 ? "active" : ""}
+              onClick={() => isLinkActive(2)}
+            >
+              Partnership
+            </li>
+            <li
+              className={linkActive === 3 ? "active" : ""}
+              onClick={() => isLinkActive(3)}
+            >
+              Testimonial
+            </li>
+          </ul>
         </div>
-    )
+      </nav>
+    </div>
+  );
 }
 
 export default NavPage;
