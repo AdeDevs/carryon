0e954a6c0b6bd289398534cbd4c11243be89bb1e
AdeDevs <adeyemiakinyemi01@gmail.com>
2024-11-16 23:06:21 +0100

    updt

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 function HomePage() {
+    const [isObjectiveActive, setIsObjectiveActive] = useState(false)
+    
     return (
         <div className="home">
             <header>
@@ -10,6 +14,43 @@ function HomePage() {
                 </div>
                 <div className="header-img"></div>
             </header>
+            <main>
+                <div className="impact">
+                    <h5>About we are - you are</h5>
+                    <div className="impact-head">
+                    <section>
+                        <h1>More People</h1>
+                        <h1>More Impact</h1>
+                    </section>
+                    <section className="impact-right">
+                        <p>
+                            Because together, we can make a real difference. Our volunteers service in a variety of roles according to their skill and interest.
+                        </p>
+                        <button>Read More</button>
+                    </section>
+                    </div>
+                    <div className="impact-foot">
+                        <section className={`objective-image ${isObjectiveActive === 0 ? "show" : ""}`} onClick={() => setIsObjectiveActive(0)}>
+                            <div className="objectives-text">
+                                <h2>Save Our Planet</h2>
+                                <p>Planting trees is not only useful in the present but also in the future...</p>
+                            </div>
+                        </section>
+                        <section className={`help-img ${isObjectiveActive === 1 ? "show" : ""}`} onClick={() => setIsObjectiveActive(1)}>
+                            <div className="objectives-text">
+                                <h2>Together We Care</h2>
+                                <p>Small acts of kindness can create a ripple effect of positive change. Working together strengthens our communities and builds a better tomorrow...</p>
+                            </div>
+                        </section>
+                        <section className={`community-img ${isObjectiveActive === 2 ? "show" : ""}`} onClick={() => setIsObjectiveActive(2)}>
+                            <div className="objectives-text">
+                                <h2>Empowering Communities</h2>
+                                <p>Every effort helps build a brighter future and your contribution helps create a brighter, more sustainable future for all...</p>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </main>
         </div>
     )
 }
