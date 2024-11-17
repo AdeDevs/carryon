334bd35e991a69cef837e367ac115e3d53bdfc79
AdeDevs <adeyemiakinyemi01@gmail.com>
2024-11-18 00:07:14 +0100

    updt

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import One from "../assets/volunteer.jpg";
 
 function HomePage() {
     const [isObjectiveActive, setIsObjectiveActive] = useState(false)
@@ -48,6 +49,84 @@ function HomePage() {
                                 <p>Every effort helps build a brighter future and your contribution helps create a brighter, more sustainable future for all...</p>
                             </div>
                         </section>
+                    </div>
+                    <div className="together-card">
+                        <h5>Get ready for now -  start by your</h5>
+                        <h2>Together we are strong</h2>
+                        <section className="stats">
+                        <div>
+                            <h1>20k</h1>
+                            <p>more people have taken part of changes</p>
+                        </div>
+                        <div>
+                            <h1>20+</h1>
+                            <p>more than 20 countries have used this service</p>
+                        </div>
+                        <div>
+                            <h1>20M</h1>
+                            <p>more than $20M has accumulated every day</p>
+                        </div>
+                        </section>
+                    </div>
+                </div>
+                <div className="community">
+                    <div className="community-stats">
+                        <h5>Open Partnership -  Start Your Journey</h5>
+                        <h1>Almost is <span className="color">never</span> enough</h1>
+                        <div className="community-list">
+                            <div>
+                                <h2><ion-icon name="globe-outline"></ion-icon></h2>
+                                <aside>
+                                    <h3>Global Community</h3>
+                                    <p>Reach a community that can make use strong and useful</p>
+                                </aside>
+                            </div>
+                            <div>
+                                <h2 className="black"><ion-icon name="man-outline"></ion-icon></h2>
+                                <aside>
+                                    <h3>Crowdfunding</h3>
+                                    <p>Affordable ceiling and are very suitable for novice funders</p>
+                                </aside>
+                            </div>
+                        </div>
+                        <button><img src={One} alt="One" /><img src={One} alt="One" /><img src={One} alt="One" /> 4K+</button>
+                        <div className="community-bar">
+                            <div className="bar-width"></div>
+                        </div>
+                    </div>
+                    <div className="community-image">
+                        <h5>Join Us</h5>
+                    </div>
+                </div>
+                <div className="testimonials">
+                    <h5>Dieter F. Uchtdorf</h5>
+                    <h4>"As we lose ourselves in the service of others, we discover our own lives and our own happiness"</h4>
+                    <div className="testimonial-box">
+                        <div className="testimonial-card">
+                            <section className="testimonial-image"></section>
+                            <section className="testimonial-info">
+                                <h5>Testimonial by hero</h5>
+                                <p>
+                                    it's been an incredible journey, i feel good about volunteering, and i wish more people can do voluntary
+                                </p>
+                                <h3>Thanks mba</h3>
+                                <h4>CTO of Apple</h4>
+                            </section>
+                        </div>
+                        <div className="testimonial-card">
+                            <section className="testimonial-image"></section>
+                            <section className="testimonial-info">
+                                <h5>Testimonial by hero</h5>
+                                <p>it's been an incredible journey, i feel good about volunteering, and i wish more people can do voluntary</p>
+                            </section>
+                        </div>
+                        <div className="testimonial-card">
+                            <section className="testimonial-image"></section>
+                            <section className="testimonial-info">
+                                <h5>Testimonial by hero</h5>
+                                <p>it's been an incredible journey, i feel good about volunteering, and i wish more people can do voluntary</p>
+                            </section>
+                        </div>
                     </div>
                 </div>
             </main>
